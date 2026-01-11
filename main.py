c29bb6fe48493de6f8bbf26873ef66f49bb40c80
kharry06 <harringtonkyle68@gmail.com>
2026-01-11 14:17:40 -0500

    Commit All

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    if response.usage_metadata == None:
+        raise RuntimeError("No usage metadata found in response")
+
+    print(f"User Prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
