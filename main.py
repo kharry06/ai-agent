42ee391ab1a3631cd746f2f1628c3792139cd845
kharry06 <harringtonkyle68@gmail.com>
2026-01-11 16:54:53 -0500

    tokens

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 def main():
     load_dotenv()
@@ -11,12 +15,12 @@ def main():
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     
     if response.usage_metadata == None:
         raise RuntimeError("No usage metadata found in response")
 
-    print(f"User Prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"User Prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
